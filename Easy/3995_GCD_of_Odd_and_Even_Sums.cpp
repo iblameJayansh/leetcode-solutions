03b6f994e340a53d42256ffe2e02e500361caa86
jayanshbharati <5ds7mhpn6s@privaterelay.appleid.com>
2026-07-16 03:40:32 +0530

    Sync LeetCode solution: GCD of Odd and Even Sums (C++)

---
+++ Easy/3995_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: GCD of Odd and Even Sums
+ * Problem ID: 3995
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 8.7 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-15
+ */
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
